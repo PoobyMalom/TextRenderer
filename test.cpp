bea3fb047ca14fb4490d686e1395ffd5480e1f34
Toby Mallon <57491618+PoobyMalom@users.noreply.github.com>
2024-08-03 17:56:08 -0400

    Move simple glyph rendering section to own method in GlyphTable.cpp

--- test.cpp
+++ test.cpp
@@ -19,7 +19,7 @@
 #include "TTFFile.h"
 using namespace std;
 
-const double SCALINGFACTOR = 1;
+const double SCALINGFACTOR = 0.05;
 
 const int ADVANCEWIDTH = 600 * SCALINGFACTOR;
 const int ADVANCEHEIGHT = 1320 * SCALINGFACTOR;
@@ -54,49 +54,6 @@ uint16_t convertEndian16(uint16_t value) {
            ((value << 8)  & 0xFF00);
 }
 
-void DrawBezier(SDL_Renderer* renderer, const SDL_Point point1, const SDL_Point controlPoint, const SDL_Point point2) {
-    // Check if the control point is collinear with the start and end points
-    auto isCollinear = [](const SDL_Point& p0, const SDL_Point& p1, const SDL_Point& p2) {
-        return std::abs((p2.y - p0.y) * (p1.x - p0.x) - (p1.y - p0.y) * (p2.x - p0.x)) < 1e-5;
-    };
-
-    if (isCollinear(point1, controlPoint, point2)) {
-        // Draw a straight line if the points are collinear
-        SDL_RenderDrawLine(renderer, point1.x, point1.y, point2.x, point2.y);
-        return;
-    }
-
-    int numPoints = 100; // Increase the number of points for a smoother curve
-    float t = 0.0;
-    float step = 1.0f / numPoints;
-    std::vector<SDL_Point> points;
-
-    for (int i = 0; i <= numPoints; i++) {
-        float x = ((1 - t) * (1 - t)) * point1.x + 2 * (1 - t) * t * controlPoint.x + (t * t) * point2.x;
-        float y = ((1 - t) * (1 - t)) * point1.y + 2 * (1 - t) * t * controlPoint.y + (t * t) * point2.y;
-        SDL_Point point = { static_cast<int>(x), static_cast<int>(y) };
-        points.push_back(point);
-        t += step;
-    }
-
-    // Line simplification: remove consecutive points that are too close to each other
-    std::vector<SDL_Point> simplifiedPoints;
-    const int distanceThreshold = 1; // Distance threshold for simplification
-    simplifiedPoints.push_back(points.front());
-
-    for (size_t i = 1; i < points.size(); ++i) {
-        if (std::abs(points[i].x - simplifiedPoints.back().x) > distanceThreshold ||
-            std::abs(points[i].y - simplifiedPoints.back().y) > distanceThreshold) {
-            simplifiedPoints.push_back(points[i]);
-        }
-    }
-
-    // Draw the simplified points
-    for (size_t i = 1; i < simplifiedPoints.size(); ++i) {
-        SDL_RenderDrawLine(renderer, simplifiedPoints[i - 1].x, simplifiedPoints[i - 1].y, simplifiedPoints[i].x, simplifiedPoints[i].y);
-    }
-}
-
 
 int main() {
     // Open the file in binary mode
@@ -119,7 +76,7 @@ int main() {
     uint16_t platformID = 0;
     uint16_t encodingID = 4;
 
-    string alph = "B";
+    string alph = "Toby is a bastard child? japer is cool though!!";
     cout << static_cast<int>(alph[0]) << endl;
 
     vector<Glyph> glyphs = ttfFile.parseGlyphs(buffer, platformID, encodingID, alph);
@@ -191,67 +148,10 @@ int main() {
             currentXOffset = 0;
             currentYOffset += ADVANCEHEIGHT;
         }
-        Glyph glyph = glyphs[i];
-        glyph.addPointsBetween();
-        vector<uint16_t> endpoints = glyph.getEndPtsOfContours();
-
-        int currentContour = 0;
-        int contourStartIndex = 0;
-
-        cout << "glyph: " << alph[i] << endl;
-        for (uint16_t endpoint : endpoints) {
-            cout << "endpoints: " << endpoint << endl;
-        }
-
-        vector<int16_t> xCoordinates = glyph.getXCoordinates();
-        vector<int16_t> yCoordinates = glyph.getYCoordinates();
-
-        vector<uint8_t> flags = glyph.getFlags();
-        for (int j = 0; j < xCoordinates.size(); ++j) {
-            uint8_t flag = flags[j];
-            if (j > endpoints[currentContour]) {
-                contourStartIndex = endpoints[currentContour] + 1;
-                ++currentContour;
-            }
-            if (flag & 1) {
-                SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-                drawCircle(renderer, xCoordinates[j], SCREEN_HEIGHT - yCoordinates[j], radius);
-            } else {
-                SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-                drawCircle(renderer, xCoordinates[j], SCREEN_HEIGHT - yCoordinates[j], radius);
-            }
-            if (j == endpoints[currentContour]) {
-                SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-                drawCircle(renderer, xCoordinates[j], SCREEN_HEIGHT - yCoordinates[j], 2);
-            }
-            cout << "point: " << j << " in contour: " << currentContour << " | x: " << xCoordinates[j] << ", y: " << yCoordinates[j] << endl;
-
-
-            //if (flag & 1) {
-            //    SDL_Point point1 = 
-            //}
-
-            if (flag & 1) { // If the current point is an on-curve point
-                SDL_Point point1 = { static_cast<int>(xCoordinates[j] * SCALINGFACTOR + currentXOffset), static_cast<int>(SCREEN_HEIGHT - yCoordinates[j] * SCALINGFACTOR + currentYOffset)};
-                SDL_Point controlPoint;
-                SDL_Point point2;
-                if (j !=  endpoints[currentContour] - 1) {
-                    cout << j << endl;
-                    controlPoint.x = static_cast<int>((xCoordinates[j + 1] + currentXOffset) * SCALINGFACTOR);
-                    controlPoint.y = static_cast<int>(SCREEN_HEIGHT - ((yCoordinates[j + 1] + currentYOffset) * SCALINGFACTOR));
-                    point2.x = static_cast<int>((xCoordinates[j + 2] + currentXOffset) * SCALINGFACTOR);
-                    point2.y = static_cast<int>(SCREEN_HEIGHT - ((yCoordinates[j + 2] + currentYOffset) * SCALINGFACTOR));
-                } else {
-                    controlPoint.x = static_cast<int>((xCoordinates[j + 1] + currentXOffset) * SCALINGFACTOR);
-                    controlPoint.y = static_cast<int>(SCREEN_HEIGHT - ((yCoordinates[j + 1] + currentYOffset) * SCALINGFACTOR));
-                    point2.x = static_cast<int>((xCoordinates[contourStartIndex] + currentXOffset) * SCALINGFACTOR);
-                    point2.y = static_cast<int>(SCREEN_HEIGHT - ((yCoordinates[contourStartIndex] + currentYOffset) * SCALINGFACTOR));
-                }
-                cout << "contour " << currentContour << " start index: " << contourStartIndex << endl;
-                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-
-                DrawBezier(renderer, point1, controlPoint, point2);
-            }
+        if (glyphs[i].getNumberOfContours() >= 0) {
+            Glyph::drawSimpleGlyph(renderer, glyphs[i], currentXOffset, currentYOffset, SCALINGFACTOR, SCREEN_HEIGHT);
+        } else {
+            cout << "glyph " << i << " is a compound glyph";
         }
         currentXOffset += ADVANCEWIDTH;
     }
